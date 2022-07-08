d892d535848c1f8fa13c5b67d7799f01efb5f142
Joe Leung <joeleung32@yahoo.com.hk>
2022-07-08 14:21:39 +0800

    Add root route

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { app } from "#src/app.js"
 
+const TEST = process.env.TEST || '-';
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+	res.send(`Hello World! ${PORT} [${TEST}]`);
+});
+
 app.listen(process.env.PORT || 3000, () => {
 	console.warn(`App listening on http://localhost:${PORT}`);
 });
