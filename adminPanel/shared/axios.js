49f8be42cd9701f209e29f9afde3bc892a7c9fff
Joe Leung <joeleung32@yahoo.com.hk>
2022-07-18 19:29:57 +0800

    Configable API endpoint timeout

--- adminPanel/shared/axios.js
+++ adminPanel/shared/axios.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
+
 export const publicAPI = axios.create({
 	baseURL: `${process.env.CP_API_ENDPOINT}api/admin/`,
-	timeout: 1000
+	timeout: process.env.CP_API_TIMEOUT
 })
 
 export let privateAPI = axios.create({
 	baseURL: `${process.env.CP_API_ENDPOINT}api/admin/`,
-	timeout: 1000
+	timeout: process.env.CP_API_TIMEOUT
 })
 export const setBearerToken = (bearerToken) => {
 	privateAPI = axios.create({
 		baseURL: `${process.env.CP_API_ENDPOINT}api/admin/`,
-		timeout: 1000,
+		timeout: process.env.CP_API_TIMEOUT,
 		headers: {
 			'Authorization': 'Bearer ' + bearerToken
 		}
